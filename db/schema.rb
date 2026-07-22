74b990a63c06c838e2b8e13d2829eea44f9594dd
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-07-21 19:52:21 -0600

    feat: add served_files table and ServedFile tracking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_21_130000) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_22_015104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,6 +41,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_21_130000) do
     t.bigint "user_id", null: false
     t.index ["user_id", "file_name"], name: "index_scroll_positions_on_user_id_and_file_name", unique: true
     t.index ["user_id"], name: "index_scroll_positions_on_user_id"
+  end
+
+  create_table "served_files", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_served_files_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
